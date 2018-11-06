f7af6a14ded1627ca7def3a7eefdb4d5e92bad79
pabissonnier <pabissonnier@gmail.com>
2018-11-06 10:10:36 +0100

    V1 will be a game on the console with no graphic elements involved. We initialise the GAME file with a main function and a welcome message.

---
+++ mcgyver/game.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+def main():
+    print("Welcome !")
+    print("Help McGyver to escape the labyrinth !")
+    print("Grab the 3 objects and find the exit to beat the guardian...")
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
