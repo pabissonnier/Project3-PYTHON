25774a71e099f16647daf7aceeb3cc7f355cb7aa
pabissonnier <pabissonnier@gmail.com>
2018-11-06 10:20:18 +0100

    The class Level is created in LEVEL file and the instance level with FILE_MAP parameter is created in GAME file

--- mcgyver/game.py
+++ mcgyver/game.py
@@ -1,13 +1,17 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
+from level import Level
+
+
 def main():
     print("Welcome !")
     print("Help McGyver to escape the labyrinth !")
     print("Grab the 3 objects and find the exit to beat the guardian...")
 
-
-
+    """ Creation of the labyrinth"""
+    level = Level()
+    level.map_generator("file_map.txt")
 
 
 
---
+++ mcgyver/level.py
@@ -0,0 +1,15 @@
+class Level:
+    def __init__(self):
+        self.labyrinth = None
+
+    def map_generator(self, file_to_open):
+        """ Loading map from file and put it into a double list """
+        with open(file_to_open, 'r') as file:
+            labyrinth = []
+            for line in file:
+                laby_line = []
+                for case in line:
+                    if case != '\n':
+                        laby_line.append(case)
+                labyrinth.append(laby_line)
+                
