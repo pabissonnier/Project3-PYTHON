a4d01dcad9bb31710013c1dcce2e4f911c232cfb
pabissonnier <pabissonnier@gmail.com>
2018-11-20 10:30:32 +0100

    Updating game.py to show the pygame welcome page

--- mcgyver/constants.py
+++ mcgyver/constants.py
@@ -1,14 +1,15 @@
 """ Constants of the game """
 
-# Images
+# elements
 image_brick = "ressource/brick.png"
 image_floor = "ressource/floor.jpg"
 image_mcgyver = "ressource/mcgyver.png"
 image_guardian = "ressource/guardian.png"
 
-# Valeurs
+# Values
 sprites = 15
 sprite_size = 40
 
-# Autres
+# Other
 title_welcome = "Help McGyver to escape the labyrinth !"
+welcome_mac = "ressource/welcome_mac.jpg"
--- mcgyver/game.py
+++ mcgyver/game.py
@@ -14,38 +14,57 @@ def main():
     pygame.display.set_caption(title_welcome)
 
     level, mcgyver = Level.map_reset()
-    
+
+    game_activated = True
+
+    while game_activated:
+        welcome = pygame.image.load(welcome_mac).convert()
+        screen.blit(welcome, (0, 0))
+
+        pygame.display.flip()
+
+        continue_game = True
+        continue_title = True
+
+        # WELCOME LOOP
+        while continue_title:
+            pygame.time.Clock().tick(30)
+
+            for event in pygame.event.get():
+                if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+                    continue_title = False
+                    continue_game = False
+                    game_activated = False
+
+                elif event.type == KEYDOWN and event.key == K_RETURN:
+                    continue_title = False
 
 
 
+        while continue_game:
 
+            pygame.time.Clock().tick(30)
 
-    continue_game = True
+            # Game starts
+            level.map_draw(screen)
+            mcgyver.move()
 
-    while continue_game:
+            # McGyver gets an object
+            mcgyver.get_object(mcgyver.pos_x, mcgyver.pos_y)
 
-        pygame.time.Clock().tick(30)
+            # McGyver gets out (or not)
+            mcgyver.mac_out(mcgyver.pos_x, mcgyver.pos_y)
 
-        # Game starts
-        level.map_draw(screen)
-        mcgyver.move()
+            # Leave game or not
+            if mcgyver.end_game is False and mcgyver.win is True:
+                print("Help McGyver to escape the labyrinth !")
+                print("Grab the 3 objects and find the exit to beat the guardian...")
+                level, mcgyver = Level.map_reset()
 
-        # McGyver gets an object
-        mcgyver.get_object(mcgyver.pos_x, mcgyver.pos_y)
+            elif mcgyver.end_game is True:
+                continue_game = False
 
-        # McGyver gets out (or not)
-        mcgyver.mac_out(mcgyver.pos_x, mcgyver.pos_y)
-
-        # Leave game or not
-        if mcgyver.end_game is False and mcgyver.win is True:
-            print("Help McGyver to escape the labyrinth !")
-            print("Grab the 3 objects and find the exit to beat the guardian...")
-            level, mcgyver = Level.map_reset()
-
-        elif mcgyver.end_game is True:
-            continue_game = False
-
-    print("GAME OVER, Bye bye !!")
+        print("GAME OVER, Bye bye !!")
 
 
 if __name__ == "__main__":
